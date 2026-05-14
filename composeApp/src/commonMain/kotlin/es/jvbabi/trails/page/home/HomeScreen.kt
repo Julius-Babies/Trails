a65a5d18c54964782c154c8f44ef6a4e0f8ce282
Julius Vincent Babies <julius@familie-babies.de>
2026-05-14 13:02:22 +0200

    feat: Simple map

--- composeApp/src/commonMain/kotlin/es/jvbabi/trails/page/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/es/jvbabi/trails/page/home/HomeScreen.kt
@@ -5,11 +5,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import org.maplibre.compose.map.MaplibreMap
 
 @Composable
 fun HomeScreen() {
     Scaffold {
         Box(Modifier.fillMaxSize()) {
+            MaplibreMap(
+                modifier = Modifier.fillMaxSize(),
+            )
         }
     }
 }
