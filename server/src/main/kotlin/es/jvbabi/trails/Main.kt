3cd8c87267a9a61ab97a5bd100db432081f9a3ce
Julius Vincent Babies <julius@familie-babies.de>
2026-05-18 20:10:35 +0200

    fix: update ENTRYPOINT syntax and enhance static web path option validation

--- server/src/main/kotlin/es/jvbabi/trails/Main.kt
+++ server/src/main/kotlin/es/jvbabi/trails/Main.kt
@@ -4,9 +4,11 @@ import com.github.ajalt.clikt.command.SuspendingCliktCommand
 import com.github.ajalt.clikt.command.main
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.optionalValue
+import com.github.ajalt.clikt.parameters.types.path
 import io.ktor.server.engine.*
 import io.ktor.server.netty.Netty
 import kotlinx.coroutines.runBlocking
+import kotlin.io.path.absolutePathString
 
 fun main(args: Array<String>) {
     runBlocking {
@@ -17,14 +19,15 @@ fun main(args: Array<String>) {
 
 class AppCommand: SuspendingCliktCommand("server") {
 
-    val staticWebPath by option("--static-web-path", help = "Path to serve static web content from").optionalValue("")
+    val staticWebPath by option("--static-web-path", help = "Path to serve static web content from")
+        .path(mustExist = true, mustBeReadable = true)
 
     override suspend fun run() {
         embeddedServer(
             factory = Netty,
             port = 8080,
             host = "0.0.0.0",
-            module = { rootModule(staticWebPath) }
+            module = { rootModule(staticWebPath?.absolutePathString()) }
         ).start(wait = true)
     }
 }
